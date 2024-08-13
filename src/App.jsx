0f7e2f84e7b70f25c75d09d0576efe85683f5ccc
Bsjaehee <bsjaehee94@gmail.com>
2024-08-13 12:32:32 +0900

    feat: 테이블 추가

--- src/App.jsx
+++ src/App.jsx
@@ -11,25 +11,25 @@ function App() {
 
   const inputCountryHandler = (event) => {
     setCountry(event.target.value);
-    console.log(country);
+    // console.log(country);
   };
   const inputGoldHandler = (event) => {
     setGold(event.target.value);
-    console.log(gold);
+    // console.log(gold);
   };
   const inputSilverHandler = (event) => {
     setSilver(event.target.value);
-    console.log(silver);
+    // console.log(silver);
   };
   const inputBronzeHandler = (event) => {
     setBronze(event.target.value);
-    console.log(bronze);
+    // console.log(bronze);
   };
 
   const handleAddCountry = (event) => {
     event.preventDefault();
     const newCountries = {
-      country: country,
+      countryName: country,
       gold: gold,
       silver: silver,
       bronze: bronze,
@@ -41,7 +41,7 @@ function App() {
 
   return (
     <div className='container'>
-      <h1>올림픽</h1>
+      <h1 style={{ textAlign: 'center' }}>2024 파리 올림픽</h1>
       <form action='#'>
         <div>
           <label htmlFor='countryName'>국가명</label>
@@ -82,6 +82,30 @@ function App() {
         <button onClick={handleAddCountry}>국가 추가</button>
         <button>업데이트</button>
       </form>
+      <table>
+        <thead>
+          <tr>
+            <th>국가명</th>
+            <th>금메달</th>
+            <th>은메달</th>
+            <th>동메달</th>
+            <th>액션</th>
+          </tr>
+        </thead>
+        <tbody>
+          {countries.map((country) => (
+            <tr>
+              <td>{country.countryName}</td>
+              <td>{country.gold}</td>
+              <td>{country.silver}</td>
+              <td>{country.bronze}</td>
+              <td>
+                <button>삭제</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
