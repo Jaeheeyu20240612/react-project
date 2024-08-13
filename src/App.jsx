7d5ec09e2db3f02723db8e48eb3833c7adafcdb9
Bsjaehee <bsjaehee94@gmail.com>
2024-08-13 16:50:36 +0900

    feat: 업데이트, 삭제 기능 추가

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,15 @@ import './App.css';
 
 function App() {
   const [countries, setCountries] = useState([]);
-  const [country, setCountry] = useState('');
 
+  const [country, setCountry] = useState('');
   const [gold, setGold] = useState('0');
   const [silver, setSilver] = useState('0');
   const [bronze, setBronze] = useState('0');
 
   const inputCountryHandler = (event) => {
     setCountry(event.target.value);
-    // console.log(country);
+    console.log('dddd', country);
   };
   const inputGoldHandler = (event) => {
     setGold(event.target.value);
@@ -28,6 +28,12 @@ function App() {
 
   const handleAddCountry = (event) => {
     event.preventDefault();
+    const existingCountry = countries.find((c) => {
+      return c.countryName === country;
+    });
+    if (existingCountry) {
+      return alert('이미 추가된 국가입니다.');
+    }
     const newCountries = {
       countryName: country,
       gold: gold,
@@ -39,10 +45,38 @@ function App() {
     console.log('countries--->', countries);
   };
 
+  const handleUpdateCountry = (event) => {
+    event.preventDefault();
+    const existCountry = countries.find((item) => {
+      return item.countryName === country;
+    });
+    if (existCountry) {
+      const updateCountry = countries.map((c) => {
+        if (c.countryName === country) {
+          return {
+            ...c,
+            gold: gold,
+            silver: silver,
+            bronze: bronze,
+          };
+        } else {
+          alert('등록되지 않은 국가입니다.');
+        }
+      });
+      setCountries(updateCountry);
+    } else {
+      alert('등록되지 않은 국가입니다.');
+    }
+  };
+
+  const handleDeleteCountry = (name) => {
+    const filteredCountries = countries.filter((c) => c.countryName !== name);
+    setCountries(filteredCountries);
+  };
   return (
     <div className='container'>
-      <h1 style={{ textAlign: 'center' }}>2024 파리 올림픽</h1>
-      <form action='#'>
+      <h1 className='title'>2024 파리 올림픽</h1>
+      <div className='input-label-wrap'>
         <div>
           <label htmlFor='countryName'>국가명</label>
           <input
@@ -80,32 +114,38 @@ function App() {
           />
         </div>
         <button onClick={handleAddCountry}>국가 추가</button>
-        <button>업데이트</button>
-      </form>
-      <table>
-        <thead>
-          <tr>
-            <th>국가명</th>
-            <th>금메달</th>
-            <th>은메달</th>
-            <th>동메달</th>
-            <th>액션</th>
-          </tr>
-        </thead>
-        <tbody>
-          {countries.map((country) => (
+        <button onClick={handleUpdateCountry}>업데이트</button>
+      </div>
+      <div className='table-wrap'>
+        <table>
+          <thead>
             <tr>
-              <td>{country.countryName}</td>
-              <td>{country.gold}</td>
-              <td>{country.silver}</td>
-              <td>{country.bronze}</td>
-              <td>
-                <button>삭제</button>
-              </td>
+              <th>국가명</th>
+              <th>금메달</th>
+              <th>은메달</th>
+              <th>동메달</th>
+              <th>액션</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {countries.map((country) => (
+              <tr>
+                <td>{country.countryName}</td>
+                <td>{country.gold}</td>
+                <td>{country.silver}</td>
+                <td>{country.bronze}</td>
+                <td>
+                  <button
+                    onClick={() => handleDeleteCountry(country.countryName)}
+                  >
+                    삭제
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
